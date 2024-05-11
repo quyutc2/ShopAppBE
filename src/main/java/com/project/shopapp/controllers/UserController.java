0361ca9f370bd1c50240d252da927796dd33672e
quyutc2 <trongquy1628@gmail.com>
2024-05-12 04:06:08 +0700

    create UserService

--- src/main/java/com/project/shopapp/controllers/UserController.java
+++ src/main/java/com/project/shopapp/controllers/UserController.java
@@ -2,7 +2,9 @@ package com.project.shopapp.controllers;
 
 import com.project.shopapp.dtos.UserDTO;
 import com.project.shopapp.dtos.UserLoginDTO;
+import com.project.shopapp.services.IUserService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -13,7 +15,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("${api.prefix}/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final IUserService userService;
     @PostMapping("/register")
     public ResponseEntity<?> createUser(
             @Valid @RequestBody UserDTO userDTO,
@@ -30,6 +34,7 @@ public class UserController {
             if(!userDTO.getPassword().equals(userDTO.getRetypePassword())){
                 return ResponseEntity.badRequest().body("Password does not macth");
             }
+            userService.createUser(userDTO);
             return ResponseEntity.ok("Register successfully");
         } catch (Exception e){
             return ResponseEntity.badRequest().body(e.getMessage());
@@ -40,7 +45,8 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<String> login(@Valid @RequestBody UserLoginDTO userLoginDTO){
         // Kiểm tra tt đăng nhập và sinh token
+        String token = userService.login(userLoginDTO.getPhoneNumber(),userLoginDTO.getPassword());
         // Trả về token trong respone
-        return ResponseEntity.ok("some token");
+        return ResponseEntity.ok("token");
     }
 }
---
+++ src/main/java/com/project/shopapp/services/UserService.java
@@ -0,0 +1,50 @@
+package com.project.shopapp.services;
+
+import com.project.shopapp.dtos.UserDTO;
+import com.project.shopapp.exceptions.DataNotFoundException;
+import com.project.shopapp.models.Role;
+import com.project.shopapp.models.User;
+import com.project.shopapp.repositories.RoleRepository;
+import com.project.shopapp.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+
+@RequiredArgsConstructor
+public class UserService implements IUserService{
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    @Override
+    public User createUser(UserDTO userDTO) throws DataNotFoundException {
+        String phoneNumber = userDTO.getPhoneNumber();
+        // ktar xem sdt co ton tai k
+        if(userRepository.existsByPhoneNumber(phoneNumber)){
+            throw new DataIntegrityViolationException("Phone number already exists");
+        }
+        //convert from UserDTO -> User
+        User newUser = User.builder()
+                .fullName(userDTO.getFullName())
+                .phoneNumber(userDTO.getPhoneNumber())
+                .password(userDTO.getPassword())
+                .adress(userDTO.getAddress())
+                .dateOfBirth(userDTO.getDateOfBirth())
+                .facebookAccountId(userDTO.getFacebookAccountId())
+                .googleAccountId(userDTO.getGoogleAccountId())
+                .build();
+        Role role = roleRepository.findById(userDTO.getRoleId())
+                .orElseThrow(() -> new DataNotFoundException("Role not found"));
+        newUser.setRole(role);
+        // Ktra nếu có accountId, k yêu cầu password
+        if (userDTO.getFacebookAccountId()==0||userDTO.getGoogleAccountId()==0){
+            String password = userDTO.getPassword();
+           // String encodedPassword = passwordEncoder.encode(password);
+            // newUser.setPassword(encodedPassword);
+        }
+        return userRepository.save(newUser);
+    }
+
+    @Override
+    public String login(String phoneNumber, String password) {
+        // security
+        return null;
+    }
+}
