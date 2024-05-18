348109cc298c232d27b81aaef7dad7ff5720f922
quyutc2 <trongquy1628@gmail.com>
2024-05-19 03:47:01 +0700

    setting spring security

--- src/main/java/com/project/shopapp/services/CategoryService.java
+++ src/main/java/com/project/shopapp/services/CategoryService.java
@@ -5,8 +5,6 @@ import com.project.shopapp.models.Category;
 import com.project.shopapp.repositories.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import java.util.List;
 
 @Service
