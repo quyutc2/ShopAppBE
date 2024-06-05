5cfce43c279d01a305ca87bd49d7b58711e5beb5
quyutc2 <trongquy1628@gmail.com>
2024-06-05 19:23:49 +0700

    multi language i18n

--- src/main/java/com/project/shopapp/controllers/CategoryController.java
+++ src/main/java/com/project/shopapp/controllers/CategoryController.java
@@ -3,16 +3,21 @@ package com.project.shopapp.controllers;
 
 import com.project.shopapp.dtos.CategoryDTO;
 import com.project.shopapp.models.Category;
+import com.project.shopapp.responses.UpdateCategoryResponse;
 import com.project.shopapp.services.CategoryService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.LocaleResolver;
 
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("${api.prefix}/categories")
@@ -21,6 +26,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CategoryController {
     private final CategoryService categoryService;
+    private final MessageSource messageSource;
+    private final LocaleResolver localeResolver;
     // Hiển thị tất cả các category => GRUD:get
     // post
     @PostMapping("")
@@ -47,11 +54,15 @@ public class CategoryController {
 
     //put
     @PutMapping("/{id}")
-    public ResponseEntity<String> updateCategories(
+    public ResponseEntity<UpdateCategoryResponse> updateCategories(
             @PathVariable Long id,
-            @Valid @RequestBody CategoryDTO categoryDTO) {
+            @Valid @RequestBody CategoryDTO categoryDTO,
+            HttpServletRequest request) {
         categoryService.updateCategory(id, categoryDTO);
-        return ResponseEntity.ok("Update category successfully");
+        Locale locale = localeResolver.resolveLocale(request);
+        return ResponseEntity.ok(UpdateCategoryResponse.builder()
+                        .message(messageSource.getMessage("category.update_category.update_successfully", null, locale))
+                .build());
     }
 
     // Delete
--- src/main/java/com/project/shopapp/controllers/UserController.java
+++ src/main/java/com/project/shopapp/controllers/UserController.java
@@ -3,23 +3,32 @@ package com.project.shopapp.controllers;
 import com.project.shopapp.dtos.UserDTO;
 import com.project.shopapp.dtos.UserLoginDTO;
 import com.project.shopapp.models.User;
+import com.project.shopapp.responses.LoginResponse;
 import com.project.shopapp.services.IUserService;
 import com.project.shopapp.services.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.http.fileupload.RequestContext;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("${api.prefix}/users")
 @RequiredArgsConstructor
 public class UserController {
     private final IUserService userService;
+    private final MessageSource messageSource;
+    private final LocaleResolver localeResolver;
     @PostMapping("/register")
     public ResponseEntity<?> createUser (
             @Valid @RequestBody UserDTO userDTO,
@@ -45,13 +54,21 @@ public class UserController {
 
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@Valid @RequestBody UserLoginDTO userLoginDTO) {
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody UserLoginDTO userLoginDTO
+            , HttpServletRequest request) {
         // Kiểm tra tt đăng nhập và sinh token
         try {
             String token = userService.login(userLoginDTO.getPhoneNumber(),userLoginDTO.getPassword());
-            return ResponseEntity.ok(token);
+            Locale locale = localeResolver.resolveLocale(request);
+            return ResponseEntity.ok(LoginResponse.builder()
+                    .message(messageSource.getMessage("user.login.login_successfully", null, locale))
+                            .token(token)
+                    .build());
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseEntity.badRequest().body(LoginResponse.builder()
+                    .message(e.getMessage())
+                    .build()
+            );
         }
     }
 }
